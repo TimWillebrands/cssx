e803f3abfc987a981429b61d78b369e34ce86ab9
Tim Willebrands <mail@timwillebrands.nl>
2024-01-13 12:32:25 +0100

    More init the before

---
+++ core/CssxParser.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Cssx;
+
+public static class Cssx
+{
+    public static string Parse(string raw)
+    {
+        var cssx = new StringBuilder();
+        int bracePrev = 0;
+
+        for (
+            var braceSrt = raw.IndexOf('{');
+            braceSrt > -1;
+            braceSrt = raw.IndexOf('{', braceSrt + 1))
+        {
+            var braceEnd = raw.IndexOf('}', braceSrt);
+            cssx.Append(raw[bracePrev..braceSrt]);
+
+            ExecFragment(raw[(braceSrt+1)..braceEnd], cssx);
+
+            bracePrev = braceEnd+1;
+        }
+
+        cssx.Append(raw[bracePrev..]);
+        return cssx.ToString();
+    }
+
+    static void ExecFragment(string fragment, StringBuilder cssx){
+        cssx.Append(fragment);
+    }
+}
--- run/Program.cs
+++ run/Program.cs
@@ -1,6 +1,19 @@
+using Cssx;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Cssx.Cssx.Parse("""
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>{"Cssx"}</title>
+  </head>
+  <body>
+    {"Hello my boi"}
+  </body>
+</html>
+"""));
 
 app.Run();
